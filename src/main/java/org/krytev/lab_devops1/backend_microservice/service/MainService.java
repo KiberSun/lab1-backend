d0621d2441ca789f3b814a19cf977d6a501fe876
ZeRoN <romul_773@mail.ru>
2024-09-04 23:23:58 +0300

    added getting data

--- src/main/java/org/krytev/lab_devops1/backend_microservice/service/MainService.java
+++ src/main/java/org/krytev/lab_devops1/backend_microservice/service/MainService.java
@@ -2,8 +2,11 @@ package org.krytev.lab_devops1.backend_microservice.service;
 
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 @Service
 public class MainService {
@@ -15,4 +18,15 @@ public class MainService {
         System.out.println(string);
     }
 
+    public String getString() {
+        try {
+            StringBuilder sb = new StringBuilder();
+            Scanner scanner = new Scanner(new File("data.txt"));
+            if (scanner.hasNextLine()) sb.append(scanner.nextLine());
+            for (; scanner.hasNextLine(); sb.append('\n').append(scanner.nextLine()));
+            return sb.toString();
+        } catch (FileNotFoundException e) {
+            return "";
+        }
+    }
 }
